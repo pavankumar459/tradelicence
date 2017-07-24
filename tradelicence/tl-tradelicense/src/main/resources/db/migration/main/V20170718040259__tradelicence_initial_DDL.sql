2b17362487c76ef1c2198f1cfaa8e7907d9c8d40
PavanWTC-PC\PavanWTC <pavan.kumar@walkingtree.tech>
2017-07-24 09:59:14 +0530

    committing the changes of the subcateory and details tables

--- tradelicence/tl-tradelicense/src/main/resources/db/migration/main/V20170718040259__tradelicence_initial_DDL.sql
+++ tradelicence/tl-tradelicense/src/main/resources/db/migration/main/V20170718040259__tradelicence_initial_DDL.sql
@@ -47,3 +47,51 @@ ALTER TABLE ONLY egtl_mstr_uom
     ADD CONSTRAINT unq_tluom_name UNIQUE (name);
 -------------------END-------------------
 
+------------------START------------------
+CREATE TABLE egtl_mstr_sub_category(
+    id bigint NOT NULL,
+    tenantId character varying NOT NULL,
+    code character varying(256) NOT NULL,
+    name character varying(256) NOT NULL,
+    businessNatureId character varying(256) NOT NULL,
+    categoryId bigint NOT NULL,
+    createdBy character varying,
+    lastModifiedBy character varying,
+    createdTime bigint,
+    lastModifiedTime bigint
+);
+
+CREATE SEQUENCE seq_egtl_mstr_sub_category;
+
+ALTER TABLE ONLY egtl_mstr_sub_category 
+	ALTER COLUMN id SET DEFAULT nextval('seq_egtl_mstr_sub_category');
+ALTER TABLE ONLY egtl_mstr_sub_category
+    ADD CONSTRAINT pk_egtl_mstr_sub_category PRIMARY KEY (id);
+ALTER TABLE ONLY egtl_mstr_sub_category
+    ADD CONSTRAINT fk_subcategory_category FOREIGN KEY (categoryId) REFERENCES egtl_mstr_category(id);
+ALTER TABLE ONLY egtl_mstr_sub_category
+    ADD CONSTRAINT unq_tlsubcategory_code UNIQUE (code);
+ALTER TABLE ONLY egtl_mstr_sub_category
+    ADD CONSTRAINT unq_tlsubcategory_name UNIQUE (name);
+-------------------END-------------------
+    
+-------------------START-------------------
+CREATE TABLE egtl_sub_category_details (
+    id bigint NOT NULL,
+    subCategoryId bigint NOT NULL,
+    feeType character varying(50) NOT NULL,
+    uomId bigint NOT NULL,
+    ratetype character varying(50) NOT NULL
+);
+
+CREATE SEQUENCE seq_egtl_sub_category_details;
+
+ALTER TABLE ONLY egtl_sub_category_details 
+	ALTER COLUMN id SET DEFAULT nextval('seq_egtl_sub_category_details');
+ALTER TABLE ONLY egtl_sub_category_details
+    ADD CONSTRAINT pk_egtl_sub_category_details PRIMARY KEY (id);
+ALTER TABLE ONLY egtl_sub_category_details
+    ADD CONSTRAINT fk_scdetails_subcategory FOREIGN KEY (subCategoryId) REFERENCES egtl_mstr_sub_category(id);
+ALTER TABLE ONLY egtl_sub_category_details
+    ADD CONSTRAINT fk_scdetails_uom FOREIGN KEY (uomId) REFERENCES egtl_mstr_uom(id);
+-------------------END-------------------
